a9b3e600ca11f2f44d39182e019e343f647a7f2d
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-08-11 00:32:32 +0100

    Add 101-quote.c Script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Return:  always 0 (Success)
+ */
+
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", message);
+	return (1);
+
+}
