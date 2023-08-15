0ca565886976bdea75e97685f8d71cee2995a32e
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-08-15 22:09:23 +0100

    Add 1-alphabet.c Script

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,15 @@
 #include "main.h"
-
-void print_alphabet()
-{	
+/**
+ * print_alphabet - prints the whole alphabet in lower case
+ * followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
 	char first = 97;
-	while(first != 123)
+
+	while (first != 123)
 	{
 		_putchar(first);
 		first++;
