82aaf98d76b676e795d3b1ffbbb1622076a63919
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-08-15 22:00:05 +0100

    Add 1-alphabet.c Script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  main - Entry point  prints the alphabet in
+ *  lower case using _putcahr onyl followed by a new line
+ *
+ *  Return: always return 0 (Sucess)
+ *
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
