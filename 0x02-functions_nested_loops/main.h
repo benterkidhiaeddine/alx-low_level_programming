e477027b4b8633bbf51a72f2e7ba48953fd38ebd
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-08-15 22:00:07 +0100

    Add main.h Script

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,11 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
-
-
+/**
+ *
+ *  print_alphabet - prints the whole of the alphabet in lower case follower by a new line
+ *
+ *  Return: void 
+ *
+ */
+void print_alphabet(void);
