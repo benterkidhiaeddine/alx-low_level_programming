9676ecb0246dd7cf3920226df9fc7535be67de84
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-08-15 21:06:25 +0100

    Add 0-putchar.c Script

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point  prints "_putchar" followed by a new line
+ *
+ *  Return: always return 0 (Sucess)
+ *
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
