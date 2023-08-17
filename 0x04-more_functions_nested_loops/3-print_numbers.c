3f0d73e807acf61c7f55829caa8950bb7e4fd417
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-08-17 23:26:50 +0100

    Add 3-print_numbers.c Script

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - print numbers 0 through 9
+*
+* Return: void
+*/
+void print_numbers(void)
+{
+	int digit;
+
+	digit = 48;
+	while (digit <= 57)
+	{
+		_putchar(digit);
+	}
+	_putchar('\n');
+}
