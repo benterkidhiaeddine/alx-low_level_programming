54052244f586d3b94c848a3cf923e9373104d7aa
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-08-30 13:54:38 +0100

    Add 6-puts2.c Script

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other char of the string
+* @str: pointer to the first char in the string.
+* Return: void
+*
+*/
+void puts2(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str = str + 2;
+	}
+	_putchar('\n');
+}
