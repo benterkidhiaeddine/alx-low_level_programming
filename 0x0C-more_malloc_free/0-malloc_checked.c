5ef20e62238577f01627809e3abf5e9b5b94295c
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-09-07 11:08:18 +0100

    Add 0-malloc_checked.c Script

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - create the amount of memory and returns the void pointer
+* to it and handles faliure
+* @b: unsigned int - number of bytes to be assigned
+* Return: void* - on sucess void pointer with b byes allocated
+* on faliure exit code 98
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(40);
+	}
+	return (ptr);
+}
