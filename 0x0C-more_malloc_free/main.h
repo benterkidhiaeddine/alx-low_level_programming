dd9e5621b752da398aa7009b5de861440469a217
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-09-08 01:23:51 +0100

    Change header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,6 @@
 #define HEADER
 
 #include <stdlib.h>
-#include <stdio.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
