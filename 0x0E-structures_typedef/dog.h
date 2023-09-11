a373bc235e8696439007225b10d99f3db21e9dfd
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-09-11 20:11:00 +0100

    Add dog.h Script

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog object containing name , owner and age
+ * @name: char* - name of the dog
+ * @owner: char* - name of the owner
+ * @age: float - age of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
