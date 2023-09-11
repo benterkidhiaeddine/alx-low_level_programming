6c80c59222896e6bc2e192a8e0291c60966242b2
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-09-11 20:20:02 +0100

    Add 1-init_dog.c Script

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+* init_dog - sets the composing variables of the dog struct
+* @d: dog* - pointer to the dog stcut we want to init
+* @name: char* - name of the dog
+* @age: float - age of the dog
+* @owner: char* - name of the owner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
