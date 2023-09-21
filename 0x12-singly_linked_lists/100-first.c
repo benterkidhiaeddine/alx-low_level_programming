a9b35537af4363611ef65de108097228ce1b1d23
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-09-21 18:46:30 +0100

    Add 100-first.c Script

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 /**
 * first - executes before main
-* Return void
+* Return: void
 */
-__attribute__ ((__constructor__))
-void first(void)
+void first(void) __attribute__ ((__constructor__))
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
