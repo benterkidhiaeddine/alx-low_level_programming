99c59a4aafa9df8dd35eddcf15bff06731cd5544
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-09-25 13:28:58 +0100

    Add 0-puts_recursion.c Script

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _puts_recursion - function that prints a string using recursion
+* followed by a new line
+* @s: char* - string to printed
+* Return: void
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+}
