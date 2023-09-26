eb752a1428e3306e8b3618adc677fb44e28afeca
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-09-26 21:59:53 +0100

    Fix bug where we weren't account for the case when adding to the an empty list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+include "lists.h"
 /**
 * insert_nodeint_at_index - insert the new node at index
 * @head: listsint_t** - pointer to pointer to the head node
@@ -12,26 +12,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *tmp;
 
-	if (head == NULL)
-		return (NULL);
-
 	if (*head == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = (*head);
+		(*head) = new_node;
+		return (new_node);
+	}
+
 	i = 0;
 	tmp = (*head);
 	while (tmp != NULL)
 	{
-		if (i == idx && idx == 0)
-		{
-			new_node->next = (*head);
-			(*head) = new_node;
-			return (new_node);
-		}
-		else if (i == idx - 1)
+		if (i == idx - 1)
 		{
 			new_node->next = tmp->next;
 			tmp->next = new_node;
