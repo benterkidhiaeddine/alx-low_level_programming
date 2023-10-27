e546354c8f0e1aa3b07f3a4042a53a9e040ef1e5
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-10-28 00:46:15 +0100

    Solve first project

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - entry point to te programme
+* @argc:  variable count
+* @argv:  variable array
+* Return: always 0
+*/
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
