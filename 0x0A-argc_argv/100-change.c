38d8f31f50bbbfc076f5b7bda91fa651f02be4e9
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-10-28 00:48:14 +0100

    Add 100-change.c Script

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main - entry point to te programme
+* @argc:  variable count
+* @argv:  variable array
+* Return: always 0
+*/
+int main(int argc, char **argv)
+{
+	int cents;
+	int coins;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	coins = 0;
+	cents = atoi(argv[1]);
+	coins += cents / 25;
+
+
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+
+	coins += cents / 5;
+	cents %= 5;
+
+	coins += cents / 2;
+	cents %= 2;
+	
+	coins += cents;
+
+	printf("%d\n", coins);
+	return (0);
+}
